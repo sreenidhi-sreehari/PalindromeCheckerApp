93628890dd7e5089bf40aafa30f1f4dd5bce36f8
sreenidhi-sreehari <sreenidhi.sreehari06@gmail.com>
2026-02-27 14:40:00 +0530

    WelcomeMessage

---
+++ App/src/PalindromeCheckerApp.java
@@ -0,0 +1,7 @@
+public class PalindromeCheckerApp {
+    public static void main(String[] args){
+        System.out.println("Welcome to the Palindrome Checker Management System");
+        System.out.println("version : 1.0");
+        System.out.println("System Initialized Successfully");
+    }
+}
