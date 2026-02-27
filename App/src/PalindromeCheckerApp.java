d02ca4c33089e1f6cd6d19bc7b7ff7e476bc458c
sreenidhi-sreehari <sreenidhi.sreehari06@gmail.com>
2026-02-27 16:20:45 +0530

    String Reverse

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,15 +1,26 @@
+import java.util.Scanner;
 public class PalindromeCheckerApp {
     public static void main(String[] args){
-        String str="madam";
-        boolean ispalindrome= true;
-        for(int i=0; i<str.length()/2; i++){
-            if (str.charAt(i) != str.charAt(str.length() - 1- i)) {
-                ispalindrome = false;
-                break;
+                Scanner scanner = new Scanner(System.in);
+
+                // Taking input from user
+                System.out.println("Enter a string to check if it is a palindrome:");
+                String originalString = scanner.nextLine();
+
+                String reversedString = "";
+
+                // Reversing the string using for loop
+                for (int i = originalString.length() - 1; i >= 0; i--) {
+                    reversedString = reversedString + originalString.charAt(i);
+                }
+
+                // Comparing original and reversed string using equals()
+                if (originalString.equals(reversedString)) {
+                    System.out.println("The given string is a Palindrome.");
+                } else {
+                    System.out.println("The given string is NOT a Palindrome.");
+                }
+
+                scanner.close();
             }
         }
-        System.out.print("Input text: "+str);
-        System.out.print("\nIs it a Palindrome? : True");
-
-    }
-}
