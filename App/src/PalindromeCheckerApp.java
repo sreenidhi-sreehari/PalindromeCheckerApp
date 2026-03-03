bbc75d9ec2d5e18a65efad15b318d85e34d75085
Sreenidhi Sreehari <sreenidhisreehari@Sreenidhis-MacBook-Air-383.local>
2026-03-03 22:40:29 +0530

    UC13

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,41 +1,43 @@
 import java.util.*;
 
-interface PalindromeStrategy {
-    boolean check(String input);
-}
-
-class StackStrategy implements PalindromeStrategy {
-    public boolean check(String input) {
-        Stack<Character> stack = new Stack<>();
-        for (char c : input.toCharArray()) stack.push(c);
-        for (char c : input.toCharArray()) {
-            if (c != stack.pop()) return false;
-        }
-        return true;
-    }
-}
-
-class DequeStrategy implements PalindromeStrategy {
-    public boolean check(String input) {
-        Deque<Character> deque = new ArrayDeque<>();
-        for (char c : input.toCharArray()) deque.add(c);
-        while (deque.size() > 1) {
-            if (!deque.removeFirst().equals(deque.removeLast())) return false;
-        }
-        return true;
-    }
-}
-
 public class PalindromeCheckerApp {
+
+    public static boolean twoPointerCheck(String input) {
+        int start = 0;
+        int end = input.length() - 1;
+        while (start < end) {
+            if (input.charAt(start) != input.charAt(end)) return false;
+            start++;
+            end--;
+        }
+        return true;
+    }
+
+    public static boolean recursiveCheck(String input, int start, int end) {
+        if (start >= end) return true;
+        if (input.charAt(start) != input.charAt(end)) return false;
+        return recursiveCheck(input, start + 1, end - 1);
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Input: ");
         String input = sc.nextLine();
 
-        PalindromeStrategy strategy = new StackStrategy();
-        boolean result = strategy.check(input);
+        long start1 = System.nanoTime();
+        boolean result1 = twoPointerCheck(input);
+        long end1 = System.nanoTime();
 
-        System.out.println("Is Palindrome? : " + result);
+        long start2 = System.nanoTime();
+        boolean result2 = recursiveCheck(input, 0, input.length() - 1);
+        long end2 = System.nanoTime();
+
+        System.out.println("Two Pointer Result: " + result1);
+        System.out.println("Execution Time: " + (end1 - start1) + " ns");
+
+        System.out.println("Recursive Result: " + result2);
+        System.out.println("Execution Time: " + (end2 - start2) + " ns");
+
         sc.close();
     }
 }
